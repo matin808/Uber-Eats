cf5d9987d342e4d7bf40b13c80b52db84c37e3c2
matin808 <matinkadri07@gmail.com>
2023-07-22 13:39:51 +0530

    Uber Eats

---
+++ App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Home from './screens/Home';
+
+function App(): JSX.Element {
+  return (
+    <View style={{flex: 1}}>
+      <Home />
+    </View>
+  );
+}
+
+export default App;
---
+++ components/HeaderTabs.tsx
@@ -0,0 +1,72 @@
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+
+const HeaderTabs = () => {
+  const [activeTab, setActiveTab] = useState<string>('Delivery');
+
+  return (
+    <View style={styles.headerTabContainer}>
+      <HeaderButton
+        title="Delivery"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        title="Pickup"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+    </View>
+  );
+};
+
+interface HeaderButtonProps {
+  title: string;
+  activeTab: any;
+  setActiveTab: any;
+}
+
+const HeaderButton = ({title, activeTab, setActiveTab}: HeaderButtonProps) => {
+  return (
+    <TouchableOpacity onPress={() => setActiveTab(title)}>
+      <View
+        style={[
+          styles.headerBtnContainer,
+          {
+            backgroundColor: title === activeTab ? 'black' : 'white',
+          },
+        ]}>
+        <Text
+          style={[
+            styles.headerTabs_Text,
+            {color: title === activeTab ? 'white' : 'black'},
+          ]}>
+          {title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerTabContainer: {
+    justifyContent: 'center',
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+
+  headerTabs_Text: {
+    fontSize: 15,
+    fontWeight: '900',
+  },
+
+  headerBtnContainer: {
+    padding: 4,
+    paddingRight: 15,
+    backgroundColor: '#fff',
+    paddingLeft: 15,
+    borderRadius: 10,
+  },
+});
+
+export default HeaderTabs;
