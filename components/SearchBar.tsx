9a82f185af8862c2c566386e296a80d06b49b029
matin808 <matinkadri07@gmail.com>
2023-07-22 16:02:16 +0530

    Added SearchBar Component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,52 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const SearchBar = () => {
   return (
     <View style={{marginTop: 15, flexDirection: 'row'}}>
-      <GooglePlacesAutocomplete placeholder="Search" />
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        query={{}}
+        styles={{
+          textInput: {
+            backgroundColor: '#eee',
+            borderRadius: 20,
+            fontWeight: '700',
+            marginTop: 7,
+          },
+          textInputContainer: {
+            backgroundColor: '#eee',
+            borderRadius: 50,
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginRight: 10,
+          },
+        }}
+        renderLeftButton={() => (
+          <View style={{marginLeft: 10}}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              marginRight: 10,
+              backgroundColor: 'white',
+              padding: 10,
+              borderRadius: 20,
+              flexDirection: 'row',
+              gap: 5,
+
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <MaterialCommunityIcons name="clock-time-four-outline" size={20} />
+            <Text style={{fontWeight: '700'}}>Search</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
